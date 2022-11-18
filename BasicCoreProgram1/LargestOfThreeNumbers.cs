7c911910ee5049cc09db63557195dd1d7cd3d8ca
dericklopis <stanforever08@gmail.com>
2022-11-18 19:45:20 +0530

    Added: The Largest of three numbers program

---
+++ BasicCoreProgram1/LargestOfThreeNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram1
+{
+    internal class LargestOfThreeNumbers
+    {
+        public static void LargestNumber() 
+        {
+            Console.WriteLine("Enter any number: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter any number: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter any number: ");
+            int C = Convert.ToInt32(Console.ReadLine());
+            if (A > B && A > C)
+            {
+                Console.WriteLine("The Largest number  " +A);
+            }
+            else if(B > C && B > C)
+            {
+                Console.WriteLine("The Largest number  " +B);
+            }
+            else
+            {
+                Console.WriteLine("The Largest number  " +C);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- BasicCoreProgram1/Program.cs
+++ BasicCoreProgram1/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCoreProgram1
         static void Main(string[] args)
         {
             Console.WriteLine("Please choose one program from below options");
-            Console.WriteLine("1:FlipCoin \n2:LeapYear \n3:PowerOf2 \n4:HarmonicNumbers \n5:Factors \n6:QuotientAndRemainder \n7:SwapTwoNumbers \n8:EvenOrOdd  \n9:VowelsOrConsonant");
+            Console.WriteLine("1:FlipCoin \n2:LeapYear \n3:PowerOf2 \n4:HarmonicNumbers \n5:Factors \n6:QuotientAndRemainder \n7:SwapTwoNumbers \n8:EvenOrOdd  \n9:VowelsOrConsonant \n10:LargestOfThreeNumbers");
             string options = Console.ReadLine();
            switch (options)
            {
@@ -41,6 +41,9 @@ namespace BasicCoreProgram1
                     break;
                 case "9":
                     VowelOrConsonant.Vowels();
+                    break;
+                case "10":
+                    LargestOfThreeNumbers.LargestNumber();
                     break;
                 default:
                     Console.WriteLine("Choose from above options only");
